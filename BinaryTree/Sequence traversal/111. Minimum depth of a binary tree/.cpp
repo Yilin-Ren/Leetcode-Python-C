05ed458a71837e2b9eee596424d1cc91a86d4af4
Yilin-Ren <u3637105@connect.hku.hk>
2025-01-21 18:53:17 +0800

    Update and rename BinaryTree/111. Minimum depth of a binary tree/.cpp to BinaryTree/Sequence traversal/111. Minimum depth of a binary tree/.cpp

--- BinaryTree/Sequence traversal/111. Minimum depth of a binary tree/.cpp
+++ BinaryTree/Sequence traversal/111. Minimum depth of a binary tree/.cpp
@@ -23,7 +23,7 @@ public:
                 que.pop();
                 if (node->left) que.push(node->left);
                 if (node->right) que.push(node->right);
-                if (!node->left && !node->right) { // 当左右孩子都为空的时候，说明是最低点的一层了，退出
+                if (!node->left && !node->right) { 
                     return depth;
                 }
             }
