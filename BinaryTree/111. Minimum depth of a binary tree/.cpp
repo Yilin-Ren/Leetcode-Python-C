6bd7eff2b95c1ea5c501faf791c0af7abde430b1
Yilin-Ren <u3637105@connect.hku.hk>
2025-01-21 18:46:00 +0800

    Create .cpp

---
+++ BinaryTree/111. Minimum depth of a binary tree/.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        queue<TreeNode*> que;
+        int depth = 0;
+        if(root != NULL) que.push(root);
+        while(!que.empty()){
+            int size = que.size();
+            depth++;
+            for(int i = 0; i<size; i++){
+                TreeNode* node = que.front();
+                que.pop();
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+                if (!node->left && !node->right) { // 当左右孩子都为空的时候，说明是最低点的一层了，退出
+                    return depth;
+                }
+            }
+        }
+        return depth;
+    }
+};
