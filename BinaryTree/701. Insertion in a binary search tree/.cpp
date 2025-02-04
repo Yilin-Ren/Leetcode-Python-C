6a9848ddf19d76abbcf1ddc5ef9b4231fd07e2d0
Yilin-Ren <u3637105@connect.hku.hk>
2025-02-04 16:54:05 +0800

    Create .cpp

---
+++ BinaryTree/701. Insertion in a binary search tree/.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == NULL){
+            TreeNode* node = new TreeNode(val);
+            return node;
+        }
+        TreeNode* cur = root;
+        TreeNode* pre  = root;
+        while(cur != NULL){
+            pre = cur;
+            if (cur->val > val) cur = cur->left;
+            else cur = cur->right;
+        }
+        TreeNode* node = new TreeNode(val);
+        if (val < pre->val) pre->left = node;
+        else pre->right = node;
+        return root;
+    }
+};
