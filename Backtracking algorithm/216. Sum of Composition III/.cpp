6642e5bd5e1937b4b94c51f62da3e31b1346776e
Yilin-Ren <u3637105@connect.hku.hk>
2025-02-06 21:11:01 +0800

    Create .cpp

---
+++ Backtracking algorithm/216. Sum of Composition III/.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(int k, int n, int sum, int startindex){
+        if(path.size()==k && sum == n){
+            result.push_back(path);
+            return;
+        }
+        
+        for(int i = startindex; i<=9; i++){
+            sum += i;
+            path.push_back(i);
+            backtracking(k, n, sum, i+1);
+            sum -= i;
+            path.pop_back(); 
+        }
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backtracking(k, n, 0, 1);
+        return result;
+    }
+};
